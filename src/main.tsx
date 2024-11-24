bcbca42fe1e7f4fdc28c8e14253f0f942484a666
Pavan Kottapalli <pavan.kottapalli@stonybrook.edu>
2024-11-23 17:34:42 -0800

    Committing releaseable code for product highlights

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { store } from 'src/store';
 import App from 'src/App.tsx';
-import { Button, ConfigProvider, theme } from 'antd';
+import { ConfigProvider, theme } from 'antd';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
